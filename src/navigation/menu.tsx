58f34e3cc897ea87ce1dbcb1592d16d21738765b
caroline santos <contato.caroline1999@gmail.com>
2026-01-08 10:17:16 -0300

    feat: created routes

---
+++ src/navigation/menu.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+
+export interface SubMenuItem {
+  name: string;
+  path: string;
+}
+
+export interface MenuCategory {
+  category: string;
+  icon: React.ReactNode;
+  items: SubMenuItem[];
+}
+
+export const menu: MenuCategory[] = [
+   {
+    category: 'Originais',
+    icon: <SportsEsportsIcon />,
+    items: [
+      { name: 'Crash', path: '/official/crash' },
+      { name: 'Dice', path: '/official/dice' },
+    ],
+  },
+  {
+    category: 'Cassino',
+    icon: <SportsEsportsIcon />,
+    items: [
+      { name: 'Slots', path: '/cassino/slots' },
+      { name: 'Roulete', path: '/cassino/roulete' },
+    ],
+  },
+  {
+    category: 'Sports',
+    icon: <SportsEsportsIcon />,
+    items: [
+      { name: 'League Of Legends', path: '/sports/leagueoflegends' },
+      { name: 'Counter Striker', path: '/sports/counterstriker' },
+    ],
+  },
+];
---
+++ src/routes/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import MainLayout from '../layout/MainLayout';
+import Home from '../pages/Home';
+import Roulete from '../pages/Cassino/Roulete';
+import Slots from '../pages/Cassino/Slots';
+import Crash from '../pages/Official/Crash';
+import Dice from '../pages/Sports/CounterStriker';
+import LeagueOfLegends from '../pages/Sports/LeagueOfLegends';
+import CounterStriker from '../pages/Official/Dice';
+
+
+const AppRoutes: React.FC = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<MainLayout><Home /></MainLayout>} />
+      <Route path="/official/crash" element={<MainLayout><Crash /></MainLayout>} />
+      <Route path="/official/dice" element={<MainLayout><Dice /></MainLayout>} />
+      <Route path="/cassino/slots" element={<MainLayout><Slots /></MainLayout>} />
+      <Route path="/cassino/roulete" element={<MainLayout><Roulete /></MainLayout>} />
+      <Route path="/cassino/slots" element={<MainLayout><Slots /></MainLayout>} />
+      <Route path="/sports/leagueoflegends" element={<MainLayout><LeagueOfLegends /></MainLayout>} />
+     <Route path="/sports/counterstriker" element={<MainLayout><CounterStriker /></MainLayout>} />
+      
+    </Routes>
+  );
+};
+
+export default AppRoutes;
